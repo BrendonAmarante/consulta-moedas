18d70c9592c79b0ce5c437bc27d67e412df19605
BrendonAmarante <brendon-amarante@live.com>
2022-04-16 17:47:25 -0300

    VALOR DO BITCOIN APARECENDO!

--- src/components/Moedas.vue
+++ src/components/Moedas.vue
@@ -1,9 +1,14 @@
 <template>
-     <div > 
-         <h1>Moedas</h1><br>    
+     <div id ="app"> 
+         <h1>Moedas</h1><br>
+            
             <input type="text" placeholder="Nome da Moeda" name="author" v-model="name">
             <button type="submit" v-on:click="adicionarMoeda" >Adicionar</button>
             <br> <br>
+            <div id="mensagem">
+               <span class="comment__author">Moeda: <strong>BTC</strong></span><br>  
+               <p>{{valorBTC}}</p>
+            </div>
             <div id="mensagem" v-for="(comment, index) in listaDeMoedas" v-bind:key="index">
                <span class="comment__author">Moeda: <strong>{{comment.name}}</strong></span><br>  
                <p>{{comment.valor}}</p>             
@@ -14,14 +19,24 @@
 </template>
 
 <script>
+import axios from "axios";
+                  
+
 export default{
     name: Comment,
     data() {
                 return {
+                    info: null,
                     moeda: [],
                     name: '',
-                    valor: ''
+                    valor: '',
+                    valorBTC: "",
+                                                 
                 }
+            },mounted() {
+                axios
+                .get("https://rest.coinapi.io/v1/exchangerate/BTC/USD", { headers: {"X-CoinAPI-Key": "82AD1ED4-EA0A-4F77-AFA2-7B894C2AA95C"}})
+                .then(response => (this.valorBTC = response.data.rate))
             },
             methods: {
                 adicionarMoeda(){
